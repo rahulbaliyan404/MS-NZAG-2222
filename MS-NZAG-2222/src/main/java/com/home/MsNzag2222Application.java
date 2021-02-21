27a20f7bf9c78eba8c540934c0d3675196a19285
rahulbaliyan <rahulbaliyan404@gmail.com>
2021-02-21 20:25:56 +0530

    This is first commit MS-NZAG-2222.

---
+++ MS-NZAG-2222/src/main/java/com/home/MsNzag2222Application.java
@@ -0,0 +1,17 @@
+package com.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MsNzag2222Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNzag2222Application.class, args);
+	}
+
+}
---
+++ MS-NZAG-2222/src/test/java/com/home/MsNzag2222ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNzag2222ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
